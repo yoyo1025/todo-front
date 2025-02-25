649aa0eeb407ba13be1ca8dd5448acd60eb451b5
yoyo1025 <hsjejxys@gmail.com>
2025-02-25 15:10:53 +0900

    GitHub認証完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import { signIn } from "@/auth"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -19,10 +19,18 @@ export default function Home() {
         <DialogContent>
           <DialogHeader>
             <DialogTitle className="flex justify-center my-5">Welcome Todo!</DialogTitle>
-            <Button className="py-2 px-4 bg-while rounded-2xl text-black font-black  hover:bg-sky-50">
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg" className="w-5 h-5"/>
-              Googleでログイン
-            </Button>
+            <form
+              action={async () => {
+                "use server"
+                await signIn("github", { callbackUrl: "/todo" })
+              }}
+              className="flex justify-center"
+            >
+              <Button className="py-2 px-4 bg-while rounded-2xl text-black font-black  hover:bg-sky-50">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" className="w-5 h-5"/>
+                GitHubでログイン
+              </Button>
+            </form>
           </DialogHeader>
         </DialogContent>
       </Dialog>
--- auth.ts
+++ auth.ts
@@ -3,4 +3,9 @@ import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return `${baseUrl}/todo`
+    },
+  },
 })
