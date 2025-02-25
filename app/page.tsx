1d84199610c938bcd96a7004ffd24f7c09e884e0
yoyo1025 <hsjejxys@gmail.com>
2025-02-25 14:04:23 +0900

    ログインボタン完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center" >
-      <Button className="py-1 px-5 bg-sky-500 rounded-2xl text-white font-black  hover:bg-sky-700">Log in</Button>
+      <Dialog>
+        <DialogTrigger>
+          <Button className="py-1 px-5 bg-sky-500 rounded-2xl text-white font-black  hover:bg-sky-700">Log in</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="flex justify-center my-5">Welcome Todo!</DialogTitle>
+            <Button className="py-2 px-4 bg-while rounded-2xl text-black font-black  hover:bg-sky-50">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg" className="w-5 h-5"/>
+              Googleでログイン
+            </Button>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
