ec10527dd77dd0f1438b5d3852252c9736965399
yoyo1025 <hsjejxys@gmail.com>
2025-02-25 13:43:58 +0900

    ボタンホバー時に色を変更

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center" >
-      <Button className="py-1 px-5 bg-sky-500 rounded-2xl text-white font-black">Log in</Button>
+      <Button className="py-1 px-5 bg-sky-500 rounded-2xl text-white font-black  hover:bg-sky-700">Log in</Button>
     </div>
   )
 }
