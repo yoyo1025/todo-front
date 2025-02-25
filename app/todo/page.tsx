a7a554f6df71ed7f9ae07d8c9ef23f6ed090e415
yoyo1025 <hsjejxys@gmail.com>
2025-02-25 17:01:27 +0900

    セッション取得&未認証の場合は/へリダイレクト

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,7 +1,19 @@
-export default function Todo() {
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
+
+export default async function Todo() {
+  const session = await auth()
+
+  console.log(session?.user?.name);
+  
+
+  if (!session) {
+    redirect("/") // 未ログインならトップページへ
+  }
+
   return (
     <div>
-      Todoページだよ
+      TODOページだよ。認証済みならここが表示されます。
     </div>
   )
 }
--- auth.ts
+++ auth.ts
@@ -4,8 +4,10 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async redirect({ url, baseUrl }) {
+    async redirect({ baseUrl }) {
       return `${baseUrl}/todo`
     },
   },
 })
+
+export { auth as middleware }
